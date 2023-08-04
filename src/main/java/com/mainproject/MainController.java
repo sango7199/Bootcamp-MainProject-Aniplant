714eb683061f210b2a603fe40cdc7934fea20ac1
San <sango7199@gmail.com>
2023-08-05 01:34:40 +0900

    interceptor 관련 자바 파일 merge 오류로 중복생성되어 삭제처리

--- src/main/java/com/mainproject/MainController.java
+++ src/main/java/com/mainproject/MainController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,4 +20,3 @@ public class MainController {
     }
 
 }
-
