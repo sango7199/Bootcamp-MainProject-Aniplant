7be26b683cd36c93e85bc0ab0cd5b94b7463d599
San <sango7199@gmail.com>
2023-08-15 12:12:59 +0900

    header 3차 수정 (세부 조정)

--- src/main/java/com/mainproject/user/vo/CustomUserDetails.java
+++ src/main/java/com/mainproject/user/vo/CustomUserDetails.java
@@ -14,11 +14,13 @@ public class CustomUserDetails implements UserDetails {
     private final String username;
     private final String password;
     private final UserVO user;
+//    private final UserRank rank;
 
     public CustomUserDetails(UserVO user) {
         this.user = user;
         this.username = user.getId();
         this.password = user.getPwd();
+//        this.rank = user.getRank();
     }
     
     @Override
@@ -40,6 +42,10 @@ public class CustomUserDetails implements UserDetails {
     
     public String getNickname() {
         return user.getNickname();
+    }
+    
+    public UserRank getRank() {
+        return user.getRank();
     }
 
     @Override
