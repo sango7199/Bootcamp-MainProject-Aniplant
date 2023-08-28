d022212c18ffc9bb02a2613776ad4e16a039408e
kyuseung <rpg8988@naver.com>
2023-08-28 10:18:07 +0900

    comments 오류 수정

--- src/main/java/com/mainproject/config/SecurityConfig.java
+++ src/main/java/com/mainproject/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.mainproject.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,34 +12,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()  // CSRF protection�쓣 鍮꾪솢�꽦�솕. AJAX瑜� �궗�슜�븷 �븣 �븘�슂�븳 �꽕�젙
-            .authorizeRequests()
-            	.antMatchers("/login.do", "/join.do").permitAll() // �쉶�썝媛��엯, 濡쒓렇�씤 �럹�씠吏� �뿀�슜
-                .antMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_PRIVACY_ADMIN") // /admin�� 愿�由ъ옄留� 媛��뒫�븯�룄濡� �꽕�젙
-                .antMatchers("/privacy_admin/**").hasAuthority("ROLE_PRIVACY_ADMIN") // /privacy_admin�� 媛쒖씤�젙蹂� 愿�由ъ옄留� 媛��뒫�븯�룄濡� �꽕�젙
-                .antMatchers("/my").authenticated() // /my�뒗 �씤利앹씠 �릺�뼱�빞 �젒�냽 媛��뒫
-                .anyRequest().permitAll()
-            .and()
-            .formLogin() // form �쓣 �넻�븳 login �솢�꽦�솕
-            	.loginPage("/user/login.do") // 濡쒓렇�씤 �럹�씠吏� URL �꽕�젙, 誘몄꽕�젙 �떆 Default 濡쒓렇�씤 �솕硫�
-            	// .loginProcessingUrl("/api/login") // 濡쒓렇�씤 泥섎━�븷 URL (�솢�씤吏� 紐⑤Ⅴ寃좎쓬 �옉�룞�븞�븿;)
-            	.defaultSuccessUrl("/index.do") // 濡쒓렇�씤 �꽦怨듭떆 湲곕낯 URL
-            	.failureForwardUrl("/fail") // 濡쒓렇�씤 �떎�뙣 URL �꽕�젙
-            .and()
-            	.logout()
-            	.logoutUrl("/user/logout.do") // 濡쒓렇�븘�썐 URL �꽕�젙
-            .and()
-            .exceptionHandling()
-        		.accessDeniedPage("/error/403"); // �젒洹� 沅뚰븳 �뾾�쓬 : error 403 �럹�씠吏� 
-        return http.build();
-    }
-	
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // 鍮꾨�踰덊샇瑜� �븞�쟾�븯寃� �븫�샇�솕�븯湲� �쐞�빐 BCryptPasswordEncoder �궗�슜
-    }
-	
-}
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable() // CSRF protection을 비활성화. AJAX를 사용할 때 필요한 설정
+				.authorizeRequests().antMatchers("/css/**", "/js/**", "/img/**").permitAll() // CSS, JS, 이미지 파일에 대한 요청을
+																								// 허용
+				.antMatchers("/login.do", "/join.do").permitAll() // 회원가입, 로그인 페이지 허용
+				.antMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_PRIVACY_ADMIN") // /admin은 관리자만 가능하도록 설정
+				.antMatchers("/privacy-admin/**").hasAuthority("ROLE_PRIVACY_ADMIN") // /privacy_admin은 개인정보 관리자만 가능하도록
+																						// 설정
+				.antMatchers("/mypage/**").authenticated() // /my는 인증이 되어야 접속 가능
+				.anyRequest().permitAll().and().formLogin() // form 을 통한 login 활성화
+				.loginPage("/user/login.do") // 로그인 페이지 URL 설정, 미설정 시 Default 로그인 화면
+				// .loginProcessingUrl("/api/login") // 로그인 처리할 URL (왜인지 모르겠음 작동안함;)
+				.defaultSuccessUrl("/index.do") // 로그인 성공시 기본 URL
+				.failureForwardUrl("/fail") // 로그인 실패 URL 설정
+				.and().logout().logoutUrl("/user/logout.do") // 로그아웃 URL 설정
+				.logoutSuccessUrl("/index.do") // 로그아웃 성공 시 이동할 URL 지정
+				.invalidateHttpSession(true).clearAuthentication(true).and().sessionManagement()
+				.invalidSessionUrl("/session-expired") // 세션 만료시
+				.and().exceptionHandling().accessDeniedPage("/error/403"); // 접근 권한 없음 : error 403 페이지
+		return http.build();
+	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // 비밀번호를 안전하게 암호화하기 위해 BCryptPasswordEncoder 사용
+	}
+
+}
