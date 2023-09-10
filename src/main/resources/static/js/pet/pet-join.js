908ddb0e082cb33d8274c383a760404672a25342
San <sango7199@gmail.com>
2023-09-11 04:13:40 +0900

    마이페이지 프론트 전체 완료

--- src/main/resources/static/js/pet/pet-join.js
+++ src/main/resources/static/js/pet/pet-join.js
@@ -1,81 +1,78 @@
 $(document).ready(function() {
 
-	$('#join_btn').on('click', function(event) {
-		let profile_picture;
-		const nameInput = $('#nameInput').val();
-		const birth_input = new Date($('#birth_input').val());
-		const adopted_at_input = new Date($('#adopted_at_input').val());
-		const currentTime = new Date();
+    $('#pet-modal-register-btn').on('click', function(event) {
+        let profile_picture;
+        const nameInput = $('#nameInput').val();
+        const birth_input = new Date($('#birth_input').val());
+        const adopted_at_input = new Date($('#adopted_at_input').val());
+        const currentTime = new Date();
 
-		if (birth_input > currentTime || birth_input > adopted_at_input) {
-			alert('생일은 현재시각이나 입양일보다 이전이어야 합니다.');
-			return;
-		}
+        function showError(message) {
+            $('#registerError').text(message);
+        }
 
-		if (adopted_at_input > currentTime) {
-			alert('입양일은 현재시각보다 이전이어야 합니다.');
-			return;
-		}
-		
-		// 이름이 비어있는지 확인합니다.
+        if (birth_input > currentTime || birth_input > adopted_at_input) {
+            showError('생일은 현재시각이나 입양일보다 이전이어야 합니다.');
+            return;
+        }
+
+        if (adopted_at_input > currentTime) {
+            showError('입양일은 현재시각보다 이전이어야 합니다.');
+            return;
+        }
+        
         if (!nameInput) {
-            alert('이름을 입력해 주세요.');
+            showError('반려친구 이름을 입력해 주세요.');
             return;
         }
 
-        // 생일이 비어있거나 유효하지 않은지 확인합니다.
         if ($('#birth_input').val() === '' || isNaN(birth_input.getTime())) {
-            alert('유효한 생일을 입력해 주세요.');
+            showError('유효한 생일을 입력해 주세요.');
             return;
         }
 
-         // 입양일이 비어있거나 유효하지 않은지 확인합니다.
-         if ($('#adopted_at_input').val() === '' || isNaN(adopted_at_input.getTime())) {
-             alert('유효한 입양일을 입력해 주세요.');
-             return;
-         }
+        if ($('#adopted_at_input').val() === '' || isNaN(adopted_at_input.getTime())) {
+            showError('유효한 입양일을 입력해 주세요.');
+            return;
+        }
 
-		event.preventDefault();
+        event.preventDefault();
 
-		const species = $('input[name=species]:checked').val();
+        const species = $('input[name=species]:checked').val();
 
-		if (species == "animal") {
-			profile_picture = "/img/pet-profile/pet.png"
-		} else if (species == "plant") {
-			profile_picture = "/img/pet-profile/plant.png"
-		} else {
-			alert("종류를 선택해주세요.");
-			return;
-		}
-		
-		
-		const petData = {
-			name: $('#nameInput').val(),
-			birth: $('#birth_input').val(),
-			adopted_at: $('#adopted_at_input').val(),
-			profile_picture: profile_picture
-		};
+        if (species == "animal") {
+            profile_picture = "/img/pet-profile/pet.png"
+        } else if (species == "plant") {
+            profile_picture = "/img/pet-profile/plant.png"
+        } else {
+            showError("반려친구의 종을 선택해주세요.");
+            return;
+        }
+        
+        const petData = {
+            name: $('#nameInput').val(),
+            birth: $('#birth_input').val(),
+            adopted_at: $('#adopted_at_input').val(),
+            profile_picture: profile_picture
+        };
 
-
-
-		$.ajax({
-			url: '/api/register-pet',
-			contentType: 'application/json',
-			data: JSON.stringify(petData),
-			type: 'POST',
-			dataType: 'json',  // Expect a JSON response
-			success: function(response) {
-				if (response.status === 'success') {
-					alert('등록이 완료되었습니다.');
-					location.href = response.redirectUrl;
-				} else {
-					alert('등록에 실패하였습니다.');
-				}
-			},
-			error: function() {
-				alert('등록에 실패하였습니다.');
-			}
-		});
-
-	});
+        $.ajax({
+            url: '/api/register-pet',
+            contentType: 'application/json',
+            data: JSON.stringify(petData),
+            type: 'POST',
+            dataType: 'json',  // Expect a JSON response
+            success: function(response) {
+                if (response.status === 'success') {
+                    alert('새로운 반려친구를 등록하였습니다.');
+                    location.href = response.redirectUrl;
+                } else {
+                    showError('반려친구 등록을 실패하였습니다.');
+                }
+            },
+            error: function() {
+                showError('반려친구 등록을 실패하였습니다.');
+            }
+        });
+    });
 });
